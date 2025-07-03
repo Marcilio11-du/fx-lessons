e2036d6d3638b1e9b93691e0bfe36631893677e3
Marcilio11-du <marcilio11du@gmail.com>
2025-07-03 22:57:37 +0100

    Adicionado um comentário aleatório do LessonFive.java

--- src/main/java/guiapp/LessonFive.java
+++ src/main/java/guiapp/LessonFive.java
@@ -14,6 +14,7 @@ public class LessonFive extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    //Comentário aleatório
 
     @Override
     public void start(Stage primaryStage) {
